8bf399d553fbeff64c0aa47554cbca3c1819592e
Kevin Galligan <kgalligan@gmail.com>
2025-02-02 23:09:37 -0500

    Adding test structure. Now time to fight with Gradle.

---
+++ core/src/jvmTest/kotlin/com/powersync/psdb/PowerSyncDatabaseTest.jvm.kt
@@ -0,0 +1,8 @@
+package com.powersync.psdb
+
+import com.powersync.DatabaseDriverFactory
+
+actual abstract class RobolectricTest actual constructor()
+
+actual val factory: DatabaseDriverFactory
+    get() = TODO("Not yet implemented")
